9aa70fb9b9d334fb33f6a17101a6437385578196
gregory <gregory.facyt@gmail.com>
2017-04-12 00:05:21 -0400

    + se añadio estructura basica de componenteal archivo square

--- app/src/js/component/square.jsx
+++ app/src/js/component/square.jsx
@@ -1,1 +1,10 @@
-square.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Square extends React.Component {
+	render () {
+
+	}
+
+	ReactDOM.render(, document.getElementById('main'));
+}
